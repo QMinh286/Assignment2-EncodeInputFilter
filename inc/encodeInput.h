cc0820417693734132a63999ca104f91808d1dd8
QMinh286 <minhvq2806@gmail.com>
2025-02-12 19:22:36 -0500

    finish rpocessArgument

--- inc/encodeInput.h
+++ inc/encodeInput.h
@@ -3,3 +3,5 @@
 #include<stdlib.h>
 
 #pragma warning(disable:4996)
+void processArgument(int argc,char*argv[],char **inputFileName,char **outputFileName, int *srecMode);
+void printUsage();
--- src/encodeInput.c
+++ src/encodeInput.c
@@ -12,7 +12,34 @@ int main(int argc,char*argv[]){
 
 void processArgument(int argc,char*argv[],char **inputFileName,char **outputFileName, int *srecMode){
 	for(int i=1;i <argc;i++){
-		if(argv)
+		if(argv[i][0]=='-'){
+			switch (argv[i][1]){
+				case 'i' :
+					*inputFileName = &argv[i][2];
+					break;
+				case 'o' :
+					*outputFileName =&argv[i][2];
+					break;
+				case 's' :
+					if (strcmp(argv[i],"-srec")){
+						srecMode = 1;
+					}
+					else{
+						printUsage();
+						exit(1);
+					}
+				case 'h' :
+					printUsage();
+					exit(0);
+				default:
+					printUsage();
+					exit(1);
+			}
+		}
+		else{
+			printUsage();
+			exit(1);
+		}
 	}
 }
 
@@ -20,7 +47,7 @@ void printUsage(){
 	printf("Usage: encodeInput Read and output the result in standard output\n");
 	printf("Usage: encodeInput -i<<inputFileName>> -o<<outputFileName>> -srec   Convert input file to output file as an S-Record\n");
 	printf("Usage: encodeInput -h Show the usage statement and exit\n");
-	printf("Usage: encodeInput -o<<inputFileName>> Convert input file to Assemble format\n");
+	printf("Usage: encodeInput -o<<outputFileName>>  take the piped standard input and format it into Assemble format and write it inside output file\n");
 	printf("Usage: encodeInput -srec -i<<inputFileName>> Convert input file to S-Record format\n");
 	printf("Usage: ls -l | encodeInput -o<<outputFileName>> -srec  take the piped standard input and format it into S-Record format and write it inside output file\n");
 }
